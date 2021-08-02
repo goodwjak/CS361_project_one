fe432c766a0719953ce5b74c3ebee157efec5e10
Jake Goodwin <jakegoodwin@tutanota.com>
2021-08-02 09:47:06 -0700

    Updated request handler to only accept application/json data.

--- rocket_server/src/main.rs
+++ rocket_server/src/main.rs
@@ -45,7 +45,7 @@ fn heatmap(pagename: &str) -> String {
  * Output: a heatmap file.
  * Description: handles heatmap requests.
  */
-#[post("/heatmap/<pagename>", data="<input>")]
+#[post("/heatmap/<pagename>",format="application/json", data="<input>")]
 fn get_heatmap_data(pagename: &str) -> String {
     format!("{}, heatmap is being generated...", pagename)
     println!("Heatmap input: {}", input);

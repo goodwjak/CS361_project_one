af0a9c4150d6202bb750982d916baa6d34f41970
Jake Goodwin <jakegoodwin@tutanota.com>
2021-08-02 07:33:36 -0700

    Updated microservice with new get functionality. Will probably change to rocket.

--- rocket_server/src/main.rs
+++ rocket_server/src/main.rs
@@ -11,14 +11,28 @@
 //new way, use all the stuff from rocket crate.
 use rocket::*;
 
+//This is just the sample one.
 #[get("/hello/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+
+/*
+ * Input: a get request with a pagename.
+ * Output: a string response.
+ * Description: A placeholder function that does a little.
+ */
+#[get("/heatmap/<pagename>")]
+fn heatmap(pagename: &str) -> String {
+    format!("{}, heatmap is being generated...", pagename)
+}
+
+
+//You have to launch the rocket to get anything done.
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![heatmap])
 }
 
 

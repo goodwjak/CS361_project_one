3aa2eede08804bf4df4ec9ab98d070cf09490e14
Jake Goodwin <jakegoodwin@tutanota.com>
2021-08-02 17:46:50 -0700

    Added new library for parsing the json data.

---
+++ rocket_server/target/debug/build/serde_json-1bc5767e15a84dca/build_script_build-1bc5767e15a84dca.d
@@ -0,0 +1,5 @@
+/home/ghost/Documents/classes/2021/summer/cs361/CS361_project_one/rocket_server/target/debug/build/serde_json-1bc5767e15a84dca/build_script_build-1bc5767e15a84dca: /home/ghost/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.66/build.rs
+
+/home/ghost/Documents/classes/2021/summer/cs361/CS361_project_one/rocket_server/target/debug/build/serde_json-1bc5767e15a84dca/build_script_build-1bc5767e15a84dca.d: /home/ghost/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.66/build.rs
+
+/home/ghost/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.66/build.rs:

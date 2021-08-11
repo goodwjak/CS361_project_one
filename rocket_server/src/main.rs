e7eec42eaff5bc56adec264ef79237937223e01c
Jake Goodwin <jakegoodwin@tutanota.com>
2021-08-11 16:18:23 -0700

    Microservice generates svg heatmap images now.

--- rocket_server/src/main.rs
+++ rocket_server/src/main.rs
@@ -10,10 +10,6 @@
 //3: Processing -> request handler invoked returns response.
 //4: Response -> rocket sends out the response.
 
-
-//This is a old way of using the crate.
-//#[macro_use] extern crate rocket;
-
 //new way, use all the stuff from rocket crate.
 //Actually they explain why they used the other way on the page.
 //URL:  https://rocket.rs/v0.5-rc/guide/overview/
@@ -22,6 +18,10 @@ extern crate serde_json;
 use rocket::*;
 use std::io;
 use serde_json::{Result, Value};
+use svg::Document;
+use svg::node::element::Path;
+use svg::node::element::path::Data;
+
 
 //#######################################
 //GLOBAL
@@ -58,12 +58,9 @@ fn index() -> &'static str {    // <- request handler
 #[post("/heatmap/<pagename>",format="application/json", data="<input>")]
 fn get_heatmap_data(pagename: &str, input: &str) -> &'static str {
     let xy_vals = parse_mouse_clicks(input);
-    //let xy_parsed = parse_json_post(input, &v);
-    //let heatmap_visual = make_heatmap(xy_vals);
+    paint_heatmap(xy_vals);
     format!("{}, heatmap is being generated...", pagename);
     println!("Heatmap input: {}", input);
-    println!("xy_vals: {}", xy_vals.len());
-    //println!("xy_parsed: {}", xy_parsed["x_vals"]);
     "TESTING"
 }
 
@@ -73,6 +70,65 @@ fn get_heatmap_data(pagename: &str, input: &str) -> &'static str {
 //#######################################
 //Functions
 //#######################################
+
+
+/*
+ * Input: a resolusion, color, size
+ * Output: a node for a svg image.
+ * Description: returns a NODE for adding to svg.
+ */
+fn paint_dot(x: u64, y: u64, dot_size: u64) -> svg::node::element::Path{
+    let data = Data::new()
+        .move_to((x, y))
+        .line_by((0, dot_size))
+        .close();
+
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "red")
+        .set("opacity", 0.25)
+        .set("stroke-width", dot_size)
+        .set("d", data);
+
+    path
+}
+
+/*
+ * Input: x_vals, y_vals, dot size, svg::document
+ * Output: svg::document
+ * Description: adds dots to the document without knowing number ahead of time.
+ */
+fn add_dots(mut xy_vals: Vec::<Coordinate>, size: u64, doc: svg::Document) -> svg::Document {
+    println!("add_dots() called ...");
+    let a = xy_vals.pop().unwrap();
+    let x: u64 = a.x;
+    let y: u64 = a.y;
+    let temp_doc = doc
+        .clone()
+        .add(paint_dot(x, y, size));
+    if xy_vals.len() > 0 {
+        let rec_doc = add_dots(xy_vals, size, temp_doc);
+        return rec_doc;
+    } 
+    else {
+        return temp_doc;
+    }
+}
+
+/*
+ * Input:
+ * Output:
+ * Description:
+ */
+fn paint_heatmap(xy_vals: Vec::<Coordinate>) {
+    let width = 1920;
+    let height = 1080;
+    let document = Document::new()
+        .set("viewBox", (0, 0,width, height));
+
+    let heatmap_doc = add_dots(xy_vals, 25, document);
+    svg::save("heatmap.svg", &heatmap_doc).unwrap();
+}
 
 
 /*
@@ -99,20 +155,6 @@ fn parse_mouse_clicks(json_data: &str) -> Vec::<Coordinate> {
     }
     //return the vector and give ownership to the calling scope.
     v
-}
-
-
-/*
- * Input: A vector of coordinats
- * Output: a visual of those coordinates.
- * Description: Makes a visual heatmap of the coodinates.
- */
-//TODO: change return type to a file or something.
-fn make_heatmap(click_points: Vec::<Coordinate>) ->u64 {
-    //TODO: Find a crate for making visual data.
-
-    let heatmap = 1;
-    heatmap
 }
 
 
